0d91b777925519691d95431112430713a5e4a670
John Yang <byjohnyang@gmail.com>
2017-11-15 17:03:12 -0800

    binding issue

--- sitebuilder.py
+++ sitebuilder.py
@@ -40,4 +40,5 @@ if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "build":
         freezer.freeze()
     else:
-        app.run(port=8000)
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port)
